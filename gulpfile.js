9fcfd414ad34db8f707714e965570cbc9512c09e
heartless007 <barryblando@gmail.com>
2017-06-12 14:20:05 +0800

    Initial Commit : HEADER SECTION 06/12/2017

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+/**
+ * Created on 6/12/2017.
+ */
+
+var gulp = require('gulp'),
+      browserSync = require('browser-sync').create(),
+      reload = browserSync.reload;
+
+gulp.task('serve', function () {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch('./assets/css/*.css', reload);
+  gulp.watch('./*.html').on('change', reload);
+});
+
+gulp.task('default', ['serve']);
