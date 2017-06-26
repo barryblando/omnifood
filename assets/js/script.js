9cd1b9c8e0b96bc6b98c1274312bc15edcb7524d
heartless007 <barryblando@gmail.com>
2017-06-26 22:49:39 +0800

    Update: Maps 06/26/2017

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,4 +89,22 @@ $(document).ready(function () {
 
   });
 
+
+  /* Maps */
+  var map = new GMaps({
+    div: '.map',
+    lat: 38.7436266,
+    lng: -9.0, 
+    zoom: 12
+  });
+
+  map.addMarker({
+    lat: 38.7436266,
+    lng: -9.1602037,
+    title: 'Lisbon',
+    infoWindow: {
+      content: '<p>Our Lisbon HQ</p>'
+    }
+  });
+
 }); // end document ready
